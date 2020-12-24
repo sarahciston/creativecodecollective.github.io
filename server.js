8d85920ccb2cc8a354885a688c03088c1716499f
Glitch (creativecodecollective) <none>
2020-12-24 03:09:43 +0000

    🕵🌷 Updated with Glitch

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 const express = require("express");
 const app = express();
 const pug = require('pug');
+const fs = require('fs');
 
 app.set('view engine', 'pug');
 
@@ -19,12 +20,33 @@ app.use(express.static("public"));
 // });
 
 app.get("/", function (req, res) {
-  res.render('index', { title: 'Creative Code Collective', message: 'Hello, Creative Code Collective!' })
+  res.render('index', { title: 'Creative Code Collective' })
+})
+
+app.get('/resources', function (req, res) {
+  res.render('resources', { title: 'Resources' })
 })
 
 app.all('/search', function (req, res) {
   //search action gets processed here
-  //res.render('')
+  console.log(req.query.do);
+  let resources = {};
+  // get all the json files from resources/
+  // loop through and fs.readFile them and parse them and add them to resources
+  // render/redirect the resources page with the relevant resources
+  fs.readFile('./resources/NatureOfCode.json', (err, data) => {
+    if (err) {
+      console.log(`Error reading file from disk: ${err}`);
+    } else {
+      const resources = JSON.parse(data);
+      console.log(resources);
+      // res.redirect('resources', {
+      //   title: 'Resources',
+      //   resources: resources
+      // });
+    }
+  });
+  // res.render('resources', { title: 'Resources'} )
 })
 
 // listen for requests :)
