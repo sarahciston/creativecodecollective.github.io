5e6fada0d21ebd96397618bff5e59fffe2bf93a9
Glitch (creativecodecollective) <none>
2021-01-19 01:49:42 +0000

    🚁🚂 Updated with Glitch

--- server.js
+++ server.js
@@ -16,27 +16,12 @@ const TempPouchDB = PouchDB.defaults();
 app.use('/db', require('express-pouchdb')(TempPouchDB));
 const db = new PouchDB("database");
 
-// let resources = {};
-// // get all the json files from resources/
-// // loop through and fs.readFile them and parse them and add them to database
-// fs.readFile('./resources/NatureOfCode.json', (err, data) => {
-//   if (err) {
-//     console.log(`Error reading file from disk: ${err}`);
-//   } else {
-//     const resources = JSON.parse(data);
-//     console.log(resources);
-//   }
-// });
+let tags = {think: new Set(), use: new Set(), make: new Set()};
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 
 function getResources(query) {
-  // no keys in query: not a search
-  if (Object.keys(query).length === 0 && query.constructor === Object) {
-    return Promise.reject(new Error('empty query'));
-  }
-  
   // filter out empty key-value pairs
   let selector = {};
   for (let key in query) {
@@ -48,8 +33,7 @@ function getResources(query) {
   if (Object.keys(selector).length === 0 && selector.constructor === Object) {
     // empty query -> get all docs
     return db.allDocs({
-      include_docs: true,
-      end_key: "_design"
+      include_docs: true
     }).then(result => {
       return result.rows.map(result => result.doc);
     }).catch(err => {
@@ -72,21 +56,45 @@ function getResources(query) {
 }
 
 app.get('/resources', function (req, res) {
-  getResources(req.query)
-    .then(result => {
-      res.render('resources', {
-        title: 'Resources',
-        results: result
-      });
-    })
-    .catch(err => {
-      res.render('resources', {
-        title: 'Resources',
-        results: null
-      })
+  // no keys in query: not a search
+  if (Object.keys(req.query).length === 0 && req.query.constructor === Object) {
+    getResources({think: "", use: "", make: ""})
+      .then(result => {
+        // add all document tags to set of tags
+        for (let doc of result) {
+          for (let field of ["think", "use", "make"]) {
+            doc[field].forEach(item => tags[field].add(item));
+          }
+        }
+        console.log(tags);
+      }).catch(err => console.log(err));
+    res.render('resources', {
+      title: 'Resources',
+      results: null,
+      tags: tags
     });
+  } else {
+    getResources(req.query)
+      .then(result => {
+        res.render('resources', {
+          title: 'Resources',
+          results: result,
+          tags: tags
+        });
+        console.log(req.query);
+      })
+      .catch(err => {
+        res.render('resources', {
+          title: 'Resources',
+          results: null,
+          tags: tags
+        });
+        console.log(err);
+      });
+  }
 });
 
+//get info about the database, number of documents, etc
 app.get('/db_info', function(req, res){
   db.info()
     .then(function(r) {
@@ -97,10 +105,52 @@ app.get('/db_info', function(req, res){
     });
 })
 
+//find all database documents
+app.get('/db_all', function(req, res){
+  var docs = []
+  var ids = []
+  db.allDocs({ include_docs: true }) //{attachments:true})
+    .then(function(result) {
+      // result = result.rows.map(result => result.id)
+      docs.push(result.rows)
+      return docs
+    })
+    .then(function(all){
+      console.log(all);
+      res.send(all)
+    })
+    .catch(function(err) {
+      console.log("err: " + err);
+    });
+});
+
+//delete IDs
+app.get('/delete_all', function(req, res){
+  var docs = []
+  var ids = []
+  db.allDocs() //db.allDocs({attachments:true})
+    .then(function(result) {
+      ids = result.rows.map(result => result.id)
+      console.log(ids);
+      return ids;
+    })
+    .then(function(ids){
+      for (var i = 0; i < ids.length; i++ ){
+        db.get(ids[i]).then(function(doc){
+          console.log(ids[i], 'done');
+          return db.remove(doc);
+    }).catch(function(err){console.log(err)});  
+  }});
+});
+
 //bulkAdd
-app.get('/bulkAdd', function(req, res) {
-  let cleanData = fs.readFileSync(""); //name of bulk add file goes here
+app.get('/bulk_add', function(req, res) {
+  let cleanData = fs.readFileSync("resources/bulkAdd.json"); 
   cleanData = JSON.parse(cleanData);
+  for (let doc of cleanData) {
+    doc._id = doc.title.toLowerCase().split(" ").join("-");
+  }
+  // console.log(cleanData);
   
   db.bulkDocs(cleanData)
     .then(function(r){
